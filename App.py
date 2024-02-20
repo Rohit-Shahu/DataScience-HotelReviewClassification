5a9f534b0cc6fb3cf6f5fd9772d48e95ff5a73f2
Rohit-Shahu <61725899+Rohit-Shahu@users.noreply.github.com>
2024-02-20 20:26:32 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import spacy
+from nltk.corpus import stopwords
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
